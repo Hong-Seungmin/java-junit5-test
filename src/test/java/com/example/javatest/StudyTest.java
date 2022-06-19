3c34a0a16f38b7af48cd87cf1d5ef836fe70804f
Seungmin <globalsh1@gmail.com>
2022-06-19 19:21:19 +0900

    add parameterizedTest

--- src/test/java/com/example/javatest/StudyTest.java
+++ src/test/java/com/example/javatest/StudyTest.java
@@ -1,6 +1,10 @@
 package com.example.javatest;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StudyTest {
 
@@ -17,4 +21,21 @@ class StudyTest {
 
         Study actual = new Study(10);
     }
+
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println(
+                "test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest(Integer message) {
+        System.out.println("message = " + message);
+    }
+
+
 }
