9dbaadca3175d3c7fe49d77daeed148c07674613
Seungmin <globalsh1@gmail.com>
2022-06-19 19:07:53 +0900

    add custom tag

--- src/main/java/com/example/javatest/Study.java
+++ src/main/java/com/example/javatest/Study.java
@@ -2,4 +2,9 @@ package com.example.javatest;
 
 public class Study {
 
+    private int limit;
+
+    public Study(int i) {
+        this.limit = i;
+    }
 }
--- src/test/java/com/example/javatest/StudyTest.java
+++ src/test/java/com/example/javatest/StudyTest.java
@@ -1,7 +1,20 @@
 package com.example.javatest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 
 class StudyTest {
 
+    @FastTest
+    @DisplayName("스터디 만들기 fast")
+    void create_new_study() {
+
+        Study actual = new Study(10);
+    }
+
+    @SlowTest
+    @DisplayName("스터디 만들기 slow")
+    void create_new_study_again() {
+
+        Study actual = new Study(10);
+    }
 }
